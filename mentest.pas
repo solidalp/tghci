2594e2cde465ab2bfa88313e8284fdd59779e48f
yoban78370-droid <yoban78370@gmail.com>
2025-11-28 17:38:57 +0100

    Add files via upload
    c est le programme avec SDL capable de changer une image lors de la mort du tamagotchi

--- mentest.pas
+++ mentest.pas
@@ -4,6 +4,8 @@ program menu;
 uses cthreads,tamago,sysutils,crt,compteur,sdl2,sdl2_image;
 
 var choix : integer;
+    temps_derniere_evolution: Integer;
+    temps_actuel: Integer;
 var tama : Ttamago;
 var window : PSDL_Window;
     renderer :PSDL_renderer; 
@@ -11,7 +13,9 @@ var window : PSDL_Window;
     rect : PSDL_Rect;
     Event: TSDL_Event;    
     Running: Boolean = True;
+    
 const Max = 100;
+      intervalle =30000;
 
 procedure ChargerTamago(var T: Ttamago; nomFichier: string);
 var
@@ -32,13 +36,16 @@ begin
 
   close(f);
 end;
+
+
 begin
 
 chargerTamago(tama,'tamagochi.txt');
-begin
 
-SDL_CreateWindowAndRenderer(500,500, SDL_WINDOW_SHOWN,@window, @Renderer);
-texture:= IMG_LoadTexture(renderer,'perso.png');
+begin
+SDL_CreateWindowAndRenderer(700,600, SDL_WINDOW_SHOWN,@window, @Renderer);
+temps_derniere_evolution := SDL_GetTicks();
+texture:= IMG_LoadTexture(renderer,'lucas.png');
 if texture = nil then 
 writeln('pas d''image', IMG_GetError);
 
@@ -46,7 +53,7 @@ writeln('pas d''image', IMG_GetError);
 New(rect);
 rect^.x :=100;
 rect^.y :=100;
-rect^.w :=200;
+rect^.w :=400;
 rect^.h :=200;
 
 TcompteurThread.create(tama.faim,tama.faim);
@@ -55,45 +62,63 @@ TcompteurThread.create(tama.bonheur,tama.bonheur);
 
 
 running := true;
-while Running do
+while Running=true do
   begin
     while SDL_PollEvent(@Event) <> 0 do
     begin
       if Event.type_ = SDL_QUITEV then
         Running := False;
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(Renderer, Texture,nil,rect);
-    SDL_RenderPresent(Renderer);
-  writeln('1,evo, 2 verif,3 nourrir,4 dormir,5 jouer');
-  readln(choix);
-   if choix = 1 then
-      evolution(tama);
-   if choix = 2 then 
-	verif(tama);
-   if choix = 3 then
-	nourrir(Max,tama);
-   if choix = 4 then
-	dormir(Max,tama);
-   if choix = 5 then
-	jouer(Max,tama);
-   if choix = 0 then
-        tama.evo := fantome;
+        begin
+       
+  
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(Renderer, Texture,nil,rect);
+        SDL_RenderPresent(Renderer);
+
+  		writeln('1,evo, 2 verif,3 nourrir,4 dormir,5 jouer');
+  		readln(choix);
+  		
+   		if choix = 1 then
+      			evolution(tama);
+   		if choix = 2 then 
+			verif(tama);
+   		if choix = 3 then
+			nourrir(Max,tama);
+   		if choix = 4 then
+			dormir(Max,tama);
+   		if choix = 5 then
+			jouer(Max,tama);
+   		if choix = 0 then
+        		tama.evo := fantome;
+
 begin
-if tama.evo=fantome then
+temps_actuel := SDL_GetTicks();
+        if (temps_actuel - temps_derniere_evolution >= intervalle) then
+        begin
+        evolution(tama);
+        temps_derniere_evolution := temps_actuel;
+        
+        end;
+        begin
+	if (tama.evo=fantome) or mort(tama)=true then
+		begin
+		SDL_DestroyTexture(texture);
+		texture:= IMG_LoadTexture(renderer,'fadi.png');
+		 SDL_RenderClear(renderer);
+        	SDL_RenderCopy(Renderer, Texture,nil,rect);
+        	SDL_RenderPresent(Renderer);
+		writeln('il est mort');
+		running := false;
+		sleep(5000);
+		break;
+		end;
 
-running := false
+        
+	end;
 end;
 end;
-
-
 end;
-
-
-
-
-
-
-
+end;
 
 end;
 Dispose(rect); 
@@ -104,8 +129,7 @@ IMG_Quit;
 SDL_Quit;
 end.
 
-{SDL_RenderCopy(renderer,texture,@rect,nil);
-SDL_RenderPresent(@renderer);}
+
 
 
 
